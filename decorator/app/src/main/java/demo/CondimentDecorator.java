14ffb13394dfb4244bc55536bbaca2e649b1f98e
nguyenbn7 <nguyenbn.email@gmail.com>
2024-06-11 15:28:59 +0700

    decorator pattern

---
+++ decorator/app/src/main/java/demo/CondimentDecorator.java
@@ -0,0 +1,20 @@
+package demo;
+
+/**
+ *
+ * @author Nguyen
+ */
+public abstract class CondimentDecorator extends Beverage {
+    protected final Beverage beverage;
+
+    public Beverage getBeverage() {
+        return beverage;
+    }
+
+    public CondimentDecorator(Beverage beverage) {
+        this.beverage = beverage;
+    }
+    
+    @Override
+    public abstract String getDescription();
+}
