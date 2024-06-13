b66996bfa1802f004e3e3d665e72ade6b4ba4307
nguyenbn7 <nguyenbn.email@gmail.com>
2024-06-13 08:41:47 +0700

    command pattern

---
+++ command/app/src/main/java/demo/CeilingFanOnCommand.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class CeilingFanOnCommand implements Command {
+
+    CeilingFan ceilingFan;
+
+    public CeilingFanOnCommand(CeilingFan ceilingFan) {
+        this.ceilingFan = ceilingFan;
+    }
+
+    @Override
+    public void execute() {
+        ceilingFan.high();
+    }
+
+}
---
+++ command/app/src/main/java/demo/NoCommand.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class NoCommand implements Command {
+
+    @Override
+    public void execute() {
+
+    }
+
+}
