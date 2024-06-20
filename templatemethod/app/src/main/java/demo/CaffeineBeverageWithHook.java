2fb2d752d4da03af2af6436ac9934d5852c2ac4a
nguyenbn7 <nguyenbn.email@gmail.com>
2024-06-20 11:17:39 +0700

    template method pattern

---
+++ templatemethod/app/src/main/java/demo/CaffeineBeverageWithHook.java
@@ -0,0 +1,29 @@
+package demo;
+
+public abstract class CaffeineBeverageWithHook {
+
+    public final void prepareRecipe() {
+        boilWater();
+        brew();
+        pourInCup();
+        if (customerWantsCondiments()) {
+            addCondiments();
+        }
+    }
+
+    public abstract void brew();
+
+    public abstract void addCondiments();
+
+    public void boilWater() {
+        System.out.println("Boiling water");
+    }
+
+    public void pourInCup() {
+        System.out.println("Pouring into cup");
+    }
+
+    public boolean customerWantsCondiments() {
+        return true;
+    }
+}
---
+++ templatemethod/app/src/main/java/demo/CoffeeWithHook.java
@@ -0,0 +1,42 @@
+package demo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CoffeeWithHook extends CaffeineBeverageWithHook {
+
+    @Override
+    public void brew() {
+        System.out.println("Dripping Coffee through filter");
+    }
+
+    @Override
+    public void addCondiments() {
+        System.out.println("Adding Sugar and Milk");
+    }
+
+    @Override
+    public boolean customerWantsCondiments() {
+        String answer = getUserInput();
+
+        return answer.toLowerCase().startsWith("y");
+    }
+
+    private String getUserInput() {
+        String answer = null;
+
+        System.out.print("Would you like milk and sugar with your coffee (y/n)? ");
+
+        var in = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            answer = in.readLine();
+        } catch (IOException ioe) {
+            System.err.println("IO error trying to read your answer");
+        }
+
+        return answer == null ? "n" : answer;
+    }
+
+}
