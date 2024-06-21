3291ed1df09688ba6d1524870d75a42b721fc976
nguyenbn7 <nguyenbn.email@gmail.com>
2024-06-21 15:01:02 +0700

    proxy pattern

---
+++ proxy/src/main/java/examples/NonOwnerInvocationHandler.java
@@ -0,0 +1,26 @@
+package examples;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class NonOwnerInvocationHandler implements InvocationHandler {
+
+    Person person;
+
+    public NonOwnerInvocationHandler(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (method.getName().startsWith("get")) {
+            return method.invoke(person, args);
+        } else if (method.getName().equals("setGeekRating")) {
+            return method.invoke(person, args);
+        } else if (method.getName().startsWith("set")) {
+            throw new IllegalAccessException();
+        }
+        return null;
+    }
+
+}
