82dd86e96c587a12d8c5609db0c9fcaf1c8dbad5
nguyenbn7 <148407637+nguyenbn7@users.noreply.github.com>
2025-03-17 16:00:26 +0700

    observer pattern

---
+++ observer/app/src/main/java/demo/CurrentConditionsDisplay.java
@@ -0,0 +1,31 @@
+package demo;
+
+/**
+ *
+ * @author Nguyen
+ */
+@SuppressWarnings("LeakingThisInConstructor")
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+    private float temperature;
+    private float humidity;
+    private final WeatherData weatherData;
+
+    public CurrentConditionsDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        this.weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Current conditions: " + temperature
+                + "F degrees and " + humidity + "% humidity");
+    }
+
+    @Override
+    public void update(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        display();
+    }
+
+}
