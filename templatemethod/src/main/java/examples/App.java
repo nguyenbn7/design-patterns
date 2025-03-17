33cbbfd10b2ec7d5023e1341427be56b6179b205
nguyenbn7 <148407637+nguyenbn7@users.noreply.github.com>
2025-03-17 16:00:30 +0700

    update template method

---
+++ templatemethod/src/main/java/examples/App.java
@@ -0,0 +1,24 @@
+package examples;
+
+public class App {
+
+    public static void main(String[] args) {
+        Tea tea = new Tea();
+        Coffee coffee = new Coffee();
+
+        System.out.println("\nMaking tea...");
+        tea.prepareRecipe();
+
+        System.out.println("\nMaking coffee...");
+        coffee.prepareRecipe();
+
+        TeaWithHook teaHook = new TeaWithHook();
+        CoffeeWithHook coffeeHook = new CoffeeWithHook();
+
+        System.out.println("\nMaking tea...");
+        teaHook.prepareRecipe();
+
+        System.out.println("\nMaking coffee...");
+        coffeeHook.prepareRecipe();
+    }
+}
