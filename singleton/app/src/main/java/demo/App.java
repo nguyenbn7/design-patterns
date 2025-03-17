23c6b6ae1d8a83592e4e5d774e5a25c45171f460
nguyenbn7 <148407637+nguyenbn7@users.noreply.github.com>
2025-03-17 16:00:27 +0700

    singleton pattern

---
+++ singleton/app/src/main/java/demo/App.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class App {
+
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        System.out.println(singleton.getDescription());
+    }
+}
